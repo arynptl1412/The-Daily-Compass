e7850d9c6e1cff649eacc7a0a330a77f4c33be03
Aryan Paresh Patel <arynptl1412@gmail.com>
2025-09-08 19:36:21 +0530

    Project Update

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -1,12 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { assets, blog_data } from '../assets/assets'
 import Navbar from '../components/Navbar'
+import Moment from 'moment'
 
 const Blog = () => {
-  return (
-    <>
-        <Navbar/>
-    </>
-  )
+
+  const {id} = useParams()
+
+  const [data, setData] = useState(null)
+
+  const fetchBlogdata = async ()=>{
+    const data = blog_data.find(item=> item._id === id)
+    setData(data)
+  }
+  useEffect(()=>{
+    fetchBlogdata()
+  }, [])
+
+  return data? (
+    <div className='relative'>
+      <img src={assets.gradientBackground} alt="" className='absolute -top-50 -z-1 opacity-50'/>
+      <Navbar/>
+      <div className='text-center mt-20 text-gray-600'>
+        <p className='text-primary py-4 font-mediumm'>Published on {Moment(data.createdAt).format('MMMM Do YYYY')}</p>
+        <h1 className='text-2xl sm:text-5xl font-sm max-w-2xl mx-auto text-gray-800'>{data.title}</h1>
+        <h2 className='my-5 max-w-lg truncate mx-auto'>{data.subTitle}</h2>
+        <p className='inline-block py-1 px-4 rounded-full mb-6 border text-sm border-primary/35 bg-primary/5 font-medium text-primary'>Michael Brown</p>
+      </div>
+
+      <div>
+        
+      </div>
+
+    </div>
+  ) : <div>Loading.......</div>
 }
 
 export default Blog
