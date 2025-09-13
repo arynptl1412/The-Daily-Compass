bdad0aaa33b7e074deb0820b0521cdf15c90dc55
Aryan Paresh Patel <arynptl1412@gmail.com>
2025-09-13 18:24:20 +0530

    Project Update

---
+++ client/src/pages/admin/Layout.jsx
@@ -0,0 +1,26 @@
+import React, { Fragment } from 'react'
+import {assets} from '../../assets/assets'
+import {Outlet, useNavigate} from 'react-router-dom'
+
+const Layout = () => {
+
+    const Navigate = useNavigate()
+
+    const logout = ()=>{
+        Navigate('/')
+    }
+  return (
+    <>
+        <div className='flex items-center justify-between py-2 h-[70px] px-4 sm:px-12 border-b border-gray-200'>
+            <img src={assets.logo} alt="" className='w-32 sm:w-40 cursor-pointer' onClick={()=> Navigate('/')}/>
+            <button onClick={logout} className='text-sm px-8 py-2 bg-primary rounded-full text-white cursor-pointer'>Logout</button>
+        </div>
+        <div className='flex h-[calc(100vh-70px)]'>
+            <div>sidebar</div>
+            <Outlet/>
+        </div>
+    </>
+  )
+}
+
+export default Layout
